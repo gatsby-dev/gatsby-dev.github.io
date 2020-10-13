3b58f024f5dfed3a7b38f0ae9ed58f3a96e7f819
minh-tai <minhtai.pv3598@gmail.com>
2020-10-14 02:39:52 +0700

    Updates

---
+++ precache-manifest.51ff5204fc5692728a0a96940fee4799.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4a0429c3cec178b445d74abb110653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b23fc6ecfbf5d4233c",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "e9efc436bcc0c4c8576a",
+    "url": "/static/css/main.043c8da7.chunk.css"
+  },
+  {
+    "revision": "41b23fc6ecfbf5d4233c",
+    "url": "/static/js/2.44994552.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.44994552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9efc436bcc0c4c8576a",
+    "url": "/static/js/main.a6fce045.chunk.js"
+  },
+  {
+    "revision": "097a7288481f5490fd19",
+    "url": "/static/js/runtime-main.59a63205.js"
+  },
+  {
+    "revision": "3ff965b647aa21770daa17bfaac79fd8",
+    "url": "/static/media/Bootstrap.3ff965b6.svg"
+  },
+  {
+    "revision": "262844afdac183b1a6214da03485c168",
+    "url": "/static/media/CSS.262844af.svg"
+  },
+  {
+    "revision": "52ee96f45f510c80e7d53df15f360902",
+    "url": "/static/media/HTML.52ee96f4.svg"
+  },
+  {
+    "revision": "a732a6e5bc69653a55d6dd81ab8d2d5e",
+    "url": "/static/media/Java.a732a6e5.svg"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/JavaScript.dd60b11e.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/static/media/ReactJS.1a36cc4d.svg"
+  },
+  {
+    "revision": "3581d5d1a87c91db84212d4bfe9581d1",
+    "url": "/static/media/Sass.3581d5d1.svg"
+  },
+  {
+    "revision": "189cac84a50a2ba9b5c6c78408970a49",
+    "url": "/static/media/avatar.189cac84.jpg"
+  },
+  {
+    "revision": "598079fe93cf59597619c16d38031312",
+    "url": "/static/media/data-table.598079fe.png"
+  },
+  {
+    "revision": "8c83a1c1da313e2be91c5b9afa24703b",
+    "url": "/static/media/header-image.8c83a1c1.jpg"
+  },
+  {
+    "revision": "89f468496a08a3d43503fd3eae3ceaf1",
+    "url": "/static/media/jQuery.89f46849.svg"
+  }
+]);
